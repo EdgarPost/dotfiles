e65d0e35c6728e8f751232009b633539a8d74d6c
Edgar Post <edgar@mediamonks.com>
2017-07-07 21:25:43 +0200

    updated install script to copy files

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 # Install Neovim
-mkdir $HOME/.config/nvim
-cp ./nvim/config/nvim/init.vim $HOME/.config/nvim
+dir=$HOME/.config/nvim
+if ! [ -d file ] ; then
+    mkdir $HOME/.config/nvim
+fi
+cp -i ./nvim/config/nvim/init.vim $HOME/.config/nvim
+nvim +PlugInstall +UpdateRemotePlugins +qa
 
+# Tmux
+cp -i ./tmux/tmux.conf $HOME/.tmux.conf
 
+# Vim
+cp -i ./vim/vimrc $HOME/.vimrc
 
