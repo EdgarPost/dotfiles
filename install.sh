9c32a23b406dcd6ea9885bc4f036da6051a040bc
Edgar Post <edgar@mediamonks.com>
2017-07-08 22:25:41 +0200

    updated installer with zsh

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ cp -i ./tmux/tmux.conf $HOME/.tmux.conf
 # Vim
 cp -i ./vim/vimrc $HOME/.vimrc
 
+# ZSH
+$ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -i ./zsh/zshrc $HOME/.zshrc
