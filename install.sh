6fa81bb9e7b09816e966514990b82adcc9dadbfb
Edgar Post <edgar@mediamonks.com>
2017-07-22 08:26:32 +0200

    Empty vimrc file

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
+pwd=$(pwd)
+
 # Install Neovim
 dir=$HOME/.config/nvim
 echo $dir
 if ! [ -d file ] ; then
     mkdir $dir
 fi
-cp -i ./nvim/config/nvim/init.vim $dir
+ln -fsv $pwd/nvim/config/nvim/init.vim $dir/init.vim
 nvim +PlugInstall +UpdateRemotePlugins +qa
 
 # Tmux
-cp -i ./tmux/tmux.conf $HOME/.tmux.conf
+ln -fsv $pwd/tmux/tmux.conf $HOME/.tmux.conf
 
 # Vim
-cp -i ./vim/vimrc $HOME/.vimrc
+ln -fsv $pwd/vim/vimrc $HOME/.vimrc
 
 # ZSH
-$ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -i ./zsh/zshrc $HOME/.zshrc
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -fsv $pwd/zsh/zshrc $HOME/.zshrc
